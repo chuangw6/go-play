6c00e6840e03d23698e08c28fa4dc2116d0999c2
Chuang Wang <chuangw@google.com>
2023-11-17 08:29:14 -0800

    test oauth

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module testnet
 
 go 1.22
+
+require google.golang.org/grpc v1.59.0
+
+require (
+	cloud.google.com/go/compute v1.23.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/oauth2 v0.11.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,19 +3,26 @@
 package main
 
 import (
+	"context"
 	"log"
-	"net"
-	"os"
+
+	"google.golang.org/grpc/credentials/oauth"
 )
 
 func main() {
-	infoLog := log.New(os.Stderr, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	ifaces, err := net.Interfaces()
+	c := oauth.NewComputeEngine()
+	meta, err := c.GetRequestMetadata(context.Background())
 	if err != nil {
-		infoLog.Println(err)
+		log.Fatal("GetRequestMetadata failed: ", err)
 	}
-	for _, iface := range ifaces {
-		infoLog.Printf("Checking up network interface: |Name: %s, hardware address: %s, flags: %s|", iface.Name, iface.HardwareAddr, iface.Flags)
-	}
+	log.Println(meta)
+	// infoLog := log.New(os.Stderr, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	// ifaces, err := net.Interfaces()
+	// if err != nil {
+	// 	infoLog.Println(err)
+	// }
+	// for _, iface := range ifaces {
+	// 	infoLog.Printf("Checking up network interface: |Name: %s, hardware address: %s, flags: %s|", iface.Name, iface.HardwareAddr, iface.Flags)
+	// }
 
 }
