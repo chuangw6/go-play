1545edb9a407f10c030e6a676432ee431d49c615
Chuang Wang <chuangw@google.com>
2023-11-28 20:14:31 -0800

    test gcs grpc

--- main.go
+++ main.go
@@ -4,25 +4,33 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
+	"os"
 
-	"google.golang.org/grpc/credentials/oauth"
+	"cloud.google.com/go/storage"
+	_ "google.golang.org/grpc/xds/googledirectpath"
 )
 
 func main() {
-	c := oauth.NewComputeEngine()
-	meta, err := c.GetRequestMetadata(context.Background())
-	if err != nil {
-		log.Fatal("GetRequestMetadata failed: ", err)
-	}
-	log.Println(meta)
-	// infoLog := log.New(os.Stderr, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	// ifaces, err := net.Interfaces()
-	// if err != nil {
-	// 	infoLog.Println(err)
-	// }
-	// for _, iface := range ifaces {
-	// 	infoLog.Printf("Checking up network interface: |Name: %s, hardware address: %s, flags: %s|", iface.Name, iface.HardwareAddr, iface.Flags)
-	// }
+	os.Setenv("STORAGE_USE_GRPC", "true")
+	os.Setenv("GOOGLE_CLOUD_ENABLE_DIRECT_PATH_XDS", "true")
 
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Read the object from bucket.
+	rc, err := client.Bucket("chuangw-play").Object("test-bucket.txt").NewReader(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rc.Close()
+	body, err := io.ReadAll(rc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("wow, the body is %s", body)
 }
